b71349a4e9a8d532fb1672f209d9d6a0472b7772
Abdulkadir Yeşilkaya <101483379+Abdulkadir2516@users.noreply.github.com>
2024-04-03 17:43:18 +0300

    Update exp_4.c

--- 25.ders_03.04.24/exp_4.c
+++ 25.ders_03.04.24/exp_4.c
@@ -7,6 +7,12 @@ int main() {
   
   double myNum = 19.984589;
   printf("%.2lf", myNum);
+
+   float f1 = 44000e-2;
+  double d1 = 12E4;
   
+  printf("%f\n", f1);
+  printf("%lf", d1);
+
   return 0;
 }
